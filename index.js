c9fa236e4f438a2401a7ee0242dc2a52f3e8fbaa
Eric Song <ericksong91@gmail.com>
2022-07-21 11:40:11 -0400

    done with assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (a, b) => a / b;
+
+console.log(divide(100,1000));
+
+const square = x => x * x;
+
+console.log(square(5));
+
+const add = (a, b) => a + b;
+
+console.log(add(3, 3));
